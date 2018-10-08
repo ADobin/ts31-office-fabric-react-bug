0b3f1268201230a53f50932517c78111f967a2a8
Alex Dobin <adobin@microsoft.com>
2018-10-08 15:53:05 -0700

    Basic repo of TypeScript 3.1 + Office Fabric React

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Link } from 'office-ui-fabric-react';
+
+export class App extends React.Component {
+    public render() {
+        return <Link />;
+    }
+}
